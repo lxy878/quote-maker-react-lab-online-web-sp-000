c937588328d99fe4b500c8df7fa5010145b9ffce
Xiaoyi Liu <lxy878@gmail.com>
2020-11-25 17:52:01 +0000

    Automatically backed up by Learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,20 +1,22 @@
 export default (state = [], action) => {
+  let index, quote;
   switch(action.type){
     case('ADD_QUOTE'):
       return [...state, action.quote]
 
     case('REMOVE_QUOTE'):
-      console.log(action.quoteId)
-      // const id = action.quoteId
-      return []
+      index = state.findIndex(q => q.id === action.quoteId)
+      return [...state.slice(0, index), ...state.slice(index+1)]
 
     case('UPVOTE_QUOTE'):
-      console.log(action.quoteId)
-      return []
+      index = state.findIndex(q => q.id === action.quoteId)
+      quote = state[index]
+      return [...state.slice(0, index), {...quote, votes: quote.votes+1 }, ...state.slice(index+1)]
 
     case('DOWNVOTE_QUOTE'):
-      console.log(action.quoteId)
-      return []
+      index = state.findIndex(q => q.id === action.quoteId)
+      quote = state[index]
+      return [...state.slice(0, index), {...quote, votes: (quote.votes > 0 ? quote.votes-1 : 0) }, ...state.slice(index+1)]
 
     default:
       return state

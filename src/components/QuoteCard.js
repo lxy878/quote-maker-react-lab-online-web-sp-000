5a8d267221f60705163abe88c5c6a6f6c00699ea
Xiaoyi Liu <lxy878@gmail.com>
2020-11-25 17:51:25 +0000

    Automatically backed up by Learn

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -14,23 +14,26 @@ const QuoteCard = (props) =>
           <button
             type="button"
             className="btn btn-primary"
+            onClick={e => props.upvoteQuote(props.quote.id)}
           >
             Upvote
           </button>
           <button
             type="button"
             className="btn btn-secondary"
+            onClick={e => props.downvoteQuote(props.quote.id)}
           >
             Downvote
           </button>
           <button
             type="button"
             className="btn btn-danger"
+            onClick={e => props.removeQuote(props.quote.id)}
           >
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        {/* <div>Votes: {Render Quote Votes}</div> */}
+        <div>Votes: {props.quote.votes}</div>
       </div>
     </div>
   </div>;
